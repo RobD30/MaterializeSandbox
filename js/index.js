fef8f89ded666c6f167c441c5cc130fc866e99d0
RobD30 <rob.dickerson.dev@gmail.com>
2018-06-04 11:18:40 -0700

    Added and initialized the Floating Action Button

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var fab = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(fab, {
+    	direction: 'left'
+    });
+  });
