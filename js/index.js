86bad587c261ee2173980ec1bdfee888057e57dd
Unknown <rob.dickerson.dev@gmail.com>
2018-06-05 13:17:30 -0700

    Fixed Mobile Collapse Navbar
    Had a typo in HTML which caused sidenav to be passed through as null.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     var fab = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(fab, {
+    var fabInst = M.FloatingActionButton.init(fab, {
     	direction: 'left'
     });
     $(".dropdown-trigger").dropdown();
-  });
+    var sideNav = document.querySelectorAll('.sidenav');
+    var sideNavInst = M.Sidenav.init(sideNav, {
+    	edge: 'right'
+    });
+});
